044e8274c0689408f1f135893e6ae5224c80ccf9
rayffer jaramillo <rayffer@hotmail.es>
2023-11-10 09:33:22 -0500

    lesson5

--- JS Fundamentals/lesson4.js
+++ JS Fundamentals/lesson4.js
@@ -11,5 +11,5 @@ console.log(result)
 //equality operators
 
 var x = 1
-console.log(x == '1')
-console.log(x === '1')
+console.log(x == '1') // just comparing the value, not the type
+console.log(x === '1') //strict comparison, the value and the data type
---
+++ JS Fundamentals/lesson5.js
@@ -0,0 +1,10 @@
+// Logical "AND"
+console.log(true && true) // All values have to be TRUE for expression to be true
+
+//Logica "OR"
+console.log(false || false)
+
+var ageIsMoreThanEighteen
+var isUSCitizen 
+var eligibilidadForDriversLicense = ageIsMoreThanEighteen && isUSCitizen
+console.log('This customer is eligible for DL: ' + eligibilidadForDriversLicense)
