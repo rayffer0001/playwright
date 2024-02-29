fbea32d70884c90413e3ad6ddb494f1c68828f01
rayffer jaramillo <rayffer@hotmail.es>
2024-02-29 16:36:15 -0500

    timeout increase2

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,8 +10,8 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 60000,
-  globalTimeout: 60000,
+  timeout: 120000,
+  globalTimeout: 120000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
